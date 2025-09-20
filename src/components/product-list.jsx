ac86681ae6ec8b437976ed3dd3a42310f9952753
RyanIaro <hei.ryan.3@gmail.com>
2025-09-20 16:08:17 +0300

    feat: products search filter

--- src/components/product-list.jsx
+++ src/components/product-list.jsx
@@ -1,6 +1,20 @@
+'use client';
+
+import { useState } from "react";
 import ProductCard from "./product-card";
 
 export default function ProductsList({ products }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = products.filter((product) => {
+    const term = searchTerm.toLowerCase();
+    const nameMatches = product.name.toLowerCase().includes(term);
+    const descriptionMatches = product.description
+      ? product.description.toLowerCase().includes(term)
+    : false;
+    return nameMatches || descriptionMatches;
+  })
+  
   return (
     <div>
       <div className="mb-6 flex justify-center">
@@ -8,10 +22,12 @@ export default function ProductsList({ products }) {
         type="text"
         placeholder="Search products"
         className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        value={searchTerm}
+        onChange={(e)=>setSearchTerm(e.target.value)}
         />
       </div>
       <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {products.map((product, key) => {
+        {filteredProducts.map((product, key) => {
           return (
             <li key={key}>
               <ProductCard product={product} />
